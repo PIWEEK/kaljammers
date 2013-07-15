1205fb9363bc59394505a825cf676c238a0d5e3a
Pablo Alba <pablo.alba@kaleidos.net>
2013-07-15 16:47:43 +0200

    Android: Move frisbee

--- android/Kaljammers/KaljammersProject/Kaljammers/src/main/java/net/kaleidos/kaljammers/GameActivity.java
+++ android/Kaljammers/KaljammersProject/Kaljammers/src/main/java/net/kaleidos/kaljammers/GameActivity.java
@@ -1,17 +1,20 @@
 package net.kaleidos.kaljammers;
 
 import org.andengine.engine.camera.Camera;
+import org.andengine.engine.handler.physics.PhysicsHandler;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.sprite.AnimatedSprite;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.entity.util.FPSLogger;
 import org.andengine.opengl.texture.ITexture;
 import org.andengine.opengl.texture.bitmap.BitmapTexture;
 import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.opengl.texture.region.TextureRegionFactory;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
 import org.andengine.ui.activity.BaseGameActivity;
 import org.andengine.util.adt.io.in.IInputStreamOpener;
 import org.andengine.util.debug.Debug;
@@ -24,9 +27,16 @@ public class GameActivity extends BaseGameActivity {
     private ITexture mTexture;
     private ITextureRegion mFaceTextureRegion;
 
+
+    private static final int CAMERA_WIDTH = 800;
+    private static final int CAMERA_HEIGHT = 480;
+
+
+    AnimatedSprite frisbee;
+
     @Override
     public EngineOptions onCreateEngineOptions() {
-        final Camera camera = new Camera(0, 0, 800, 480);
+        final Camera camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
 
         return new EngineOptions(true, ScreenOrientation.LANDSCAPE_SENSOR, new FillResolutionPolicy(), camera);
     }
@@ -39,7 +49,7 @@ public class GameActivity extends BaseGameActivity {
                 @Override
                 public InputStream open() throws IOException {
 
-                    return getResources().openRawResource(R.drawable.ic_launcher);
+                    return getResources().openRawResource(R.drawable.frisbee);
                 }
             });
 
@@ -59,8 +69,12 @@ public class GameActivity extends BaseGameActivity {
 
         final Scene scene = new Scene();
         scene.setBackground(new Background(0.09804f, 0.6274f, 0.8784f));
-        final Sprite face = new Sprite(200, 200, this.mFaceTextureRegion, this.getVertexBufferObjectManager());
-        scene.attachChild(face);
+        final float centerX = (GameActivity.CAMERA_WIDTH - this.mFaceTextureRegion.getWidth()) / 2;
+        final float centerY = (GameActivity.CAMERA_HEIGHT - this.mFaceTextureRegion.getHeight()) / 2;
+
+        final Frisbee frisbee = new Frisbee(centerX, centerY, this.mFaceTextureRegion, this.getVertexBufferObjectManager());
+
+        scene.attachChild(frisbee);
 
         onCreateSceneCallback.onCreateSceneFinished(scene);
     }
@@ -69,4 +83,44 @@ public class GameActivity extends BaseGameActivity {
     public void onPopulateScene(Scene scene, OnPopulateSceneCallback onPopulateSceneCallback) throws Exception {
         onPopulateSceneCallback.onPopulateSceneFinished();
     }
+
+
+
+
+    // ===========================================================
+    // Inner and Anonymous Classes
+    // ===========================================================
+
+    private static class Frisbee extends Sprite {
+        private final PhysicsHandler mPhysicsHandler;
+        private float velX = 500f;
+        private float velY = 500f;
+
+        public Frisbee(final float pX, final float pY, final ITextureRegion pTextureRegion, final VertexBufferObjectManager pVertexBufferObjectManager) {
+            super(pX, pY, pTextureRegion, pVertexBufferObjectManager);
+            this.mPhysicsHandler = new PhysicsHandler(this);
+            this.registerUpdateHandler(this.mPhysicsHandler);
+            this.mPhysicsHandler.setVelocity(velX, velY);
+
+        }
+
+        @Override
+        protected void onManagedUpdate(final float pSecondsElapsed) {
+            if(this.mX < 0) {
+                this.mPhysicsHandler.setVelocityX(velX);
+            } else if(this.mX + this.getWidth() > GameActivity.CAMERA_WIDTH) {
+                this.mPhysicsHandler.setVelocityX(-velX);
+            }
+
+            if(this.mY < 0) {
+                this.mPhysicsHandler.setVelocityY(velY);
+            } else if(this.mY + this.getHeight() > GameActivity.CAMERA_HEIGHT) {
+                this.mPhysicsHandler.setVelocityY(-velY);
+            }
+
+            super.onManagedUpdate(pSecondsElapsed);
+        }
+    }
+
+
 }
