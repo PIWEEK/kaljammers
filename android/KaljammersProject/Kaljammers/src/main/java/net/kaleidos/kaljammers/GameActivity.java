6e611da0305d8da1cfaf0e61070e7e377c6b85f7
Pablo Alba <pablo.alba@kaleidos.net>
2013-07-16 12:36:08 +0200

    Add buttons

--- android/KaljammersProject/Kaljammers/src/main/java/net/kaleidos/kaljammers/GameActivity.java
+++ android/KaljammersProject/Kaljammers/src/main/java/net/kaleidos/kaljammers/GameActivity.java
@@ -4,7 +4,6 @@ import android.graphics.Typeface;
 import android.opengl.GLES20;
 
 import org.andengine.engine.camera.Camera;
-import org.andengine.engine.camera.hud.controls.AnalogOnScreenControl;
 import org.andengine.engine.camera.hud.controls.BaseOnScreenControl;
 import org.andengine.engine.camera.hud.controls.DigitalOnScreenControl;
 import org.andengine.engine.handler.physics.PhysicsHandler;
@@ -43,10 +42,15 @@ public class GameActivity extends BaseGameActivity {
     private ITextureRegion mPlayerTextureRegion;
     private ITextureRegion mBackgroundTextureRegion;
 
-    private DigitalOnScreenControl mDigitalOnScreenControl;
+
     private BitmapTextureAtlas mOnScreenControlTexture;
     private ITextureRegion mOnScreenControlBaseTextureRegion;
     private ITextureRegion mOnScreenControlKnobTextureRegion;
+
+    private BitmapTextureAtlas mOnScreenButton1Texture;
+    private ITextureRegion mOnScreenButton1TextureRegion;
+    private BitmapTextureAtlas mOnScreenButton2Texture;
+    private ITextureRegion mOnScreenButton2TextureRegion;
 
 
 
@@ -57,6 +61,25 @@ public class GameActivity extends BaseGameActivity {
     private static final int LAYER_FRISBEE = 2;
     private static final int LAYER_TEXT = 3;
     private static final int LAYER_COUNT = 4;
+
+
+    private static final int FIELD_LIMIT_UP = 50;
+
+
+    private static final int MOVE_NONE = 0;
+    private static final int MOVE_UP = 10;
+    private static final int MOVE_UP_RIGHT = 11;
+    private static final int MOVE_RIGHT = 1;
+    private static final int MOVE_DOWN_RIGHT = 21;
+    private static final int MOVE_DOWN = 20;
+    private static final int MOVE_DOWN_LEFT = 22;
+    private static final int MOVE_LEFT = 2;
+    private static final int MOVE_UP_LEFT = 12;
+
+
+
+    int lastMove = MOVE_NONE;
+
 
     Font mFont;
 
@@ -107,6 +130,17 @@ public class GameActivity extends BaseGameActivity {
         this.mOnScreenControlKnobTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromResource(this.mOnScreenControlTexture, this, R.drawable.onscreen_control_knob, 128, 0);
         this.mOnScreenControlTexture.load();
 
+
+        this.mOnScreenButton1Texture = new BitmapTextureAtlas(this.getTextureManager(), 64, 64, TextureOptions.BILINEAR);
+        this.mOnScreenButton2Texture = new BitmapTextureAtlas(this.getTextureManager(), 64, 64, TextureOptions.BILINEAR);
+        this.mOnScreenButton1TextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromResource(this.mOnScreenButton1Texture, this, R.drawable.button1, 0, 0);
+        this.mOnScreenButton2TextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromResource(this.mOnScreenButton2Texture, this, R.drawable.button2, 0, 0);
+
+        this.mOnScreenButton1Texture.load();
+        this.mOnScreenButton2Texture.load();
+
+
+
         this.mFont = FontFactory.create(this.getFontManager(), this.getTextureManager(), 256, 256, Typeface.create(Typeface.DEFAULT, Typeface.BOLD), 32);
 
         this.mFont.load();
@@ -132,13 +166,6 @@ public class GameActivity extends BaseGameActivity {
 
         onCreateResourcesCallback.onCreateResourcesFinished();
     }
-
-
-
-
-
-
-
 
 
     @Override
@@ -170,7 +197,7 @@ public class GameActivity extends BaseGameActivity {
         player = new Player(0, centerY, this.mPlayerTextureRegion, this.getVertexBufferObjectManager());
         scene.getChildByIndex(LAYER_PLAYER).attachChild(player);
 
-        debugText = new Text(0, 0, this.mFont, "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", new TextOptions(HorizontalAlign.LEFT), this.getVertexBufferObjectManager());
+        debugText = new Text(0, 100, this.mFont, "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", new TextOptions(HorizontalAlign.LEFT), this.getVertexBufferObjectManager());
         scene.getChildByIndex(LAYER_TEXT).attachChild(debugText);
 
         initOnScreenControls();
@@ -181,54 +208,79 @@ public class GameActivity extends BaseGameActivity {
 
 
     private void initOnScreenControls() {
-        final AnalogOnScreenControl analogOnScreenControl = new AnalogOnScreenControl(0, CAMERA_HEIGHT - this.mOnScreenControlBaseTextureRegion.getHeight(), this.camera, this.mOnScreenControlBaseTextureRegion, this.mOnScreenControlKnobTextureRegion, 0.1f, this.getVertexBufferObjectManager(), new AnalogOnScreenControl.IAnalogOnScreenControlListener() {
+        final DigitalOnScreenControl digitalOnScreenControl = new DigitalOnScreenControl(0, CAMERA_HEIGHT - this.mOnScreenControlBaseTextureRegion.getHeight(), this.camera, this.mOnScreenControlBaseTextureRegion, this.mOnScreenControlKnobTextureRegion, 0.1f, this.getVertexBufferObjectManager(), new DigitalOnScreenControl.IOnScreenControlListener() {
             @Override
             public void onControlChange(final BaseOnScreenControl pBaseOnScreenControl, final float pValueX, final float pValueY) {
-                GameActivity.this.debugText.setText("X="+pValueX+"   Y="+pValueY);
+
+
+                GameActivity.this.lastMove = MOVE_NONE;
 
                 float x = GameActivity.this.player.getX();
                 float y = GameActivity.this.player.getY();
                 if(pValueX > 0) { //RIGHT
                     x = x + GameActivity.this.player.getVelX();
+                    GameActivity.this.lastMove += MOVE_RIGHT;
                 } else if(pValueX < 0) { //LEFT
                     x = x - GameActivity.this.player.getVelX();
+                    GameActivity.this.lastMove += MOVE_LEFT;
                 }
 
                 if(pValueY > 0) { //DOWN
                     y = y + GameActivity.this.player.getVelY();
-                } else if(pValueY < 0) {
+                    GameActivity.this.lastMove += MOVE_DOWN;
+                } else if(pValueY < 0) { //UP
                     y = y - GameActivity.this.player.getVelY();
+                    GameActivity.this.lastMove += MOVE_UP;
                 }
+
+                GameActivity.this.debugText.setText("MOVE = "+GameActivity.this.lastMove);
+
                 GameActivity.this.player.setPosition(x, y);
             }
 
-            @Override
-            public void onControlClick(final AnalogOnScreenControl pAnalogOnScreenControl) {
-				/* Nothing. */
-            }
         });
-        analogOnScreenControl.getControlBase().setBlendFunction(GLES20.GL_SRC_ALPHA, GLES20.GL_ONE_MINUS_SRC_ALPHA);
-        analogOnScreenControl.getControlBase().setAlpha(0.5f);
-        analogOnScreenControl.refreshControlKnobPosition();
+        digitalOnScreenControl.setAllowDiagonal(true);
+        digitalOnScreenControl.getControlBase().setBlendFunction(GLES20.GL_SRC_ALPHA, GLES20.GL_ONE_MINUS_SRC_ALPHA);
+        digitalOnScreenControl.getControlBase().setAlpha(0.5f);
+        digitalOnScreenControl.refreshControlKnobPosition();
 
-        this.scene.setChildScene(analogOnScreenControl);
+        this.scene.setChildScene(digitalOnScreenControl);
 
 
 
 
         /* Create the button and add it to the scene. */
-        final Sprite face = new ButtonSprite(centerX, centerY, this.mFace1TextureRegion, this.mFace2TextureRegion, this.mFace3TextureRegion, this.getVertexBufferObjectManager(), this);
-        scene.registerTouchArea(face);
-        scene.attachChild(face);
+        final Sprite button1 = new ButtonSprite(CAMERA_WIDTH - 150, CAMERA_HEIGHT - this.mOnScreenButton1TextureRegion.getHeight(),
+                this.mOnScreenButton1TextureRegion, this.mOnScreenButton1TextureRegion, this.mOnScreenButton1TextureRegion, this.getVertexBufferObjectManager(),
+                new org.andengine.entity.sprite.ButtonSprite.OnClickListener() {
+
+                    @Override
+                    public void onClick(ButtonSprite buttonSprite, float v, float v2) {
+                        GameActivity.this.debugText.setText("Button 1");
+                    }
+                }
+        );
+        scene.registerTouchArea(button1);
+        scene.getChildByIndex(LAYER_TEXT).attachChild(button1);
+
+
+
+        /* Create the button and add it to the scene. */
+        final Sprite button2 = new ButtonSprite(CAMERA_WIDTH - 75, CAMERA_HEIGHT - this.mOnScreenButton2TextureRegion.getHeight(),
+                this.mOnScreenButton2TextureRegion, this.mOnScreenButton2TextureRegion, this.mOnScreenButton2TextureRegion, this.getVertexBufferObjectManager(),
+                new org.andengine.entity.sprite.ButtonSprite.OnClickListener() {
+
+                    @Override
+                    public void onClick(ButtonSprite buttonSprite, float v, float v2) {
+                        GameActivity.this.debugText.setText("Button 2");
+                    }
+                }
+        );
+        scene.registerTouchArea(button2);
+        scene.getChildByIndex(LAYER_TEXT).attachChild(button2);
+
+
         scene.setTouchAreaBindingOnActionDownEnabled(true);
-
-
-
-
-
-
-
-
 
 
 
@@ -262,14 +314,18 @@ public class GameActivity extends BaseGameActivity {
         @Override
         protected void onManagedUpdate(final float pSecondsElapsed) {
             if(this.mX < 0) {
+                this.mX = 0;
                 this.mPhysicsHandler.setVelocityX(velX);
             } else if(this.mX + this.getWidth() > GameActivity.CAMERA_WIDTH) {
+                this.mX = GameActivity.CAMERA_WIDTH - this.getWidth();
                 this.mPhysicsHandler.setVelocityX(-velX);
             }
 
-            if(this.mY < 0) {
+            if(this.mY < FIELD_LIMIT_UP) {
+                this.mY = FIELD_LIMIT_UP;
                 this.mPhysicsHandler.setVelocityY(velY);
             } else if(this.mY + this.getHeight() > GameActivity.CAMERA_HEIGHT) {
+                this.mY = GameActivity.CAMERA_HEIGHT - this.getHeight();
                 this.mPhysicsHandler.setVelocityY(-velY);
             }
 
@@ -279,45 +335,7 @@ public class GameActivity extends BaseGameActivity {
 
 
 
-    private static class Player extends Sprite {
-        private final PhysicsHandler mPhysicsHandler;
 
-        private float getVelX() {
-            return velX;
-        }
-
-        private float getVelY() {
-            return velY;
-        }
-
-        private float velX = 20f;
-        private float velY = 20f;
-
-        public Player(final float pX, final float pY, final ITextureRegion pTextureRegion, final VertexBufferObjectManager pVertexBufferObjectManager) {
-            super(pX, pY, pTextureRegion, pVertexBufferObjectManager);
-            this.mPhysicsHandler = new PhysicsHandler(this);
-            this.registerUpdateHandler(this.mPhysicsHandler);
-            this.mPhysicsHandler.setVelocity(0, 0);
-
-        }
-
-        @Override
-        protected void onManagedUpdate(final float pSecondsElapsed) {
-            if(this.mX < 0) {
-                this.mX = 0;
-            } else if(this.mX + this.getWidth() > GameActivity.CAMERA_WIDTH) {
-                this.mX = GameActivity.CAMERA_WIDTH - this.getWidth();
-            }
-
-            if(this.mY < 0) {
-                this.mY = 0;
-            } else if(this.mY + this.getHeight() > GameActivity.CAMERA_HEIGHT) {
-                this.mY = GameActivity.CAMERA_HEIGHT - this.getHeight();
-            }
-
-            super.onManagedUpdate(pSecondsElapsed);
-        }
-    }
 
 
 }
--- android/KaljammersProject/Kaljammers/src/main/java/net/kaleidos/kaljammers/Player.java
+++ android/KaljammersProject/Kaljammers/src/main/java/net/kaleidos/kaljammers/Player.java
@@ -1,8 +1,47 @@
 package net.kaleidos.kaljammers;
 
-import org.andengine.entity.Entity;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.opengl.texture.region.ITextureRegion;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
 
-public class Player extends Entity {
+public class Player extends Sprite {
 
+    private static final int FIELD_LIMIT_UP = 50;
+    private static final int FIELD_LIMIT_DOWN = 480;
+    private static final int FIELD_LIMIT_RIGHT = 395;
+    private static final int FIELD_LIMIT_LEFT = 0;
+
+    public float getVelX() {
+        return velX;
+    }
+
+    public float getVelY() {
+        return velY;
+    }
+
+    private float velX = 20f;
+    private float velY = 20f;
+
+    public Player(final float pX, final float pY, final ITextureRegion pTextureRegion, final VertexBufferObjectManager pVertexBufferObjectManager) {
+        super(pX, pY, pTextureRegion, pVertexBufferObjectManager);
+
+    }
+
+    @Override
+    protected void onManagedUpdate(final float pSecondsElapsed) {
+        if(this.mX < FIELD_LIMIT_LEFT) {
+            this.mX = FIELD_LIMIT_LEFT;
+        } else if(this.mX + this.getWidth() > FIELD_LIMIT_RIGHT) {
+            this.mX = FIELD_LIMIT_RIGHT - this.getWidth();
+        }
+
+        if(this.mY < FIELD_LIMIT_UP) {
+            this.mY = FIELD_LIMIT_UP;
+        } else if(this.mY + this.getHeight() > FIELD_LIMIT_DOWN) {
+            this.mY = FIELD_LIMIT_DOWN - this.getHeight();
+        }
+
+        super.onManagedUpdate(pSecondsElapsed);
+    }
 }
 
